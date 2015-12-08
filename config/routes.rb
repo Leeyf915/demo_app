2cf17c1c96131146bdaeff97c77208be7196049e
saasbook <saasbook@saasbook.(none)>
2015-12-08 03:50:57 -0400

    Re.....

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 DemoApp::Application.routes.draw do
-  get "pages/about"
+  match '/contact', :to=>'pages#contact'
+  match '/about', :to=>'pages#about'
+  match '/help', :to=>'pages#help'
+  root :to=>'pages#home'
   ...
 
 	
